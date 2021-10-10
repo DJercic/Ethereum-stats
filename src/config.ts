3e9732b335e247afe6396d19456d163e42695e3b
Domagoj <djercic@gmail.com>
2021-10-10 09:53:57 +0200

    Load dotenv

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,14 @@
+import dotenv from 'dotenv';
+
+dotenv.config({ path: process.env.ENV_FILE });
+
 export function need(name: string): string {
   /**
    * Mark env variable required if requested using this function.
    */
-
   const envVar = process.env[name];
   if (!envVar) {
-    throw Error(`Env variable {name} is required!`);
+    throw Error(`Env variable ${name} is required!`);
   }
   return envVar;
 }
