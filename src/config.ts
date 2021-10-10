3adb9d8f5c789d25b1f12e509f77a73e792a91d3
Domagoj <djercic@gmail.com>
2021-10-10 21:32:47 +0200

    Add read function

--- src/config.ts
+++ src/config.ts
@@ -6,9 +6,21 @@ export function need(name: string): string {
   /**
    * Mark env variable required if requested using this function.
    */
-  const envVar = process.env[name];
+  const envVar = read(name);
   if (!envVar) {
     throw Error(`Env variable ${name} is required!`);
   }
   return envVar;
+}
+
+export function read(
+  name: string,
+  defaultValue: string = undefined
+): string | undefined {
+  /**
+   * Read env variable. If variable is not set return default value.
+   *
+   */
+  const envVar = process.env[name];
+  return envVar ? envVar : defaultValue;
 }
