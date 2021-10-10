118762f5b6352f3a3d7f277d21a76a80785d0e96
Domagoj <djercic@gmail.com>
2021-10-10 22:37:47 +0200

    Fix lint errors

--- src/services/db.service.ts
+++ src/services/db.service.ts
@@ -1,30 +1,16 @@
-import { createConnection, Connection } from 'typeorm';
+import { Connection, createConnection } from 'typeorm';
+
 import { need, read } from '../config';
 
-export default class DbService {
-  public static async setup(): Promise<Connection> {
-    const connection = await createConnection({
-      name: read('DB_CONNECTION_NAME', 'default'),
-      type: 'postgres',
-      host: need('DB_HOST'),
-      port: Number(read('DB_PORT', '3306')),
-      username: need('DB_USER'),
-      password: need('DB_PASS'),
-      database: need('DB_NAME'),
-    });
-    return connection;
-  }
+export async function setup(): Promise<Connection> {
+  const connection = await createConnection({
+    name: read('DB_CONNECTION_NAME', 'default'),
+    type: 'postgres',
+    host: need('DB_HOST'),
+    port: Number(read('DB_PORT', '3306')),
+    username: need('DB_USER'),
+    password: need('DB_PASS'),
+    database: need('DB_NAME'),
+  });
+  return connection;
 }
-
-// export async function initialize(): Promise<Connection> {
-//   const connections = await createConnection({
-//     name: read('DB_CONNECTION_NAME', 'default'),
-//     type: 'postgres',
-//     host: need('DB_HOST'),
-//     port: Number(read('DB_PORT', '3306')),
-//     username: need('DB_USER'),
-//     password: need('DB_PASS'),
-//     database: need('DB_NAME'),
-//   });
-//   return connections;
-// }
