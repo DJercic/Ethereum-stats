57b7704b74ca60e909041d9d3a27f5068e2a3e34
Domagoj <djercic@gmail.com>
2021-10-10 23:13:52 +0200

    Update default port

--- src/services/db.service.ts
+++ src/services/db.service.ts
@@ -7,7 +7,7 @@ export async function setup(): Promise<Connection> {
     name: read('DB_CONNECTION_NAME', 'default'),
     type: 'postgres',
     host: need('DB_HOST'),
-    port: Number(read('DB_PORT', '3306')),
+    port: Number(read('DB_PORT', '5432')),
     username: need('DB_USER'),
     password: need('DB_PASS'),
     database: need('DB_NAME'),
