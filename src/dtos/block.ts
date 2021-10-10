e366d54bf454d9654b8e12b22ba64831fcb6c0b4
Domagoj <djercic@gmail.com>
2021-10-10 22:37:01 +0200

    Add readonly

--- src/dtos/block.ts
+++ src/dtos/block.ts
@@ -1,54 +1,54 @@
-export interface Block {
+export type Block = {
   /*
    Modeled based on https://web3js.readthedocs.io/en/v1.5.2/web3-eth.html#id59
    */
 
   /* The block number. null if a pending block. */
-  number?: Number;
+  readonly number?: number;
   /* Timestamp of the block creation */
-  timestamp: Number; //1633795408,
+  readonly timestamp: number; //1633795408,
   /* Hash of the block. null if a pending block. */
-  hash?: string;
+  readonly hash?: string;
   /* Hash of the parent block */
-  parentHash: string;
+  readonly parentHash: string;
   /* 
   Used as a multiplier to determine total transaction value.
   Determined by the network and the utilization of the last block.
   https://www.blocknative.com/blog/eip-1559-fees
   totalValue = gasUsed * (baseFeePerGas + tip)
   */
-  baseFeePerGas: string;
+  readonly baseFeePerGas: string;
   /* Integer of the difficulty for this block. */
-  difficulty: string;
+  readonly difficulty: string;
   /* Any extra data associated with the transaction */
-  extraData: string;
+  readonly extraData: string;
   /* Max that can be spend on the transaction */
-  gasLimit: Number;
+  readonly gasLimit: number;
   /* Actual gas used */
-  gasUsed: Number;
+  readonly gasUsed: number;
   /* The bloom filter for the logs of the block. null if a pending block. */
-  logsBloom: string;
+  readonly logsBloom: string;
   /* Address of the miner who mined the block */
-  miner: string;
+  readonly miner: string;
   /* */
-  mixHash: string;
+  readonly mixHash: string;
   /* Hash of the generated proof-of-work. null if a pending block */
-  nonce?: string;
+  readonly nonce?: string;
   /* */
-  receiptsRoot: string;
+  readonly receiptsRoot: string;
   /* */
-  sha3Uncles: string;
+  readonly sha3Uncles: string;
   /* Integer the size of this block in bytes. */
-  size: Number;
+  readonly size: number;
   /* */
-  stateRoot: string;
+  readonly stateRoot: string;
   /* Integer of the total difficulty of the chain until this block. */
-  totalDifficulty: string;
+  readonly totalDifficulty: string;
   /* Transaction hashes that were confirmed with the block. */
-  transactions: Array<string>;
+  readonly transactions: ReadonlyArray<string>;
   /* The root of the transaction trie of the block. */
-  transactionsRoot: string;
+  readonly transactionsRoot: string;
   /* All the other miners that computed the hash but message came in after the block was excepted. 
   Block can have max of 7 uncles. */
-  uncles: Array<string>;
-}
+  readonly uncles: ReadonlyArray<string>;
+};
