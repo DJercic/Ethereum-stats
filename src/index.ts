a26b2ff120ecc18fe264a516ccc8c8840a60304b
Domagoj <djercic@gmail.com>
2021-10-10 09:54:16 +0200

    Fetch last 10 blocks and write them to files

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,25 @@
-import log from './services/logging';
+import log from './services/logging.service';
+import { fetchUntil } from './services/ethereum.service';
+
 log.info('Starting ethereum stats service');
+
+const fs = require('fs');
+const { promisify } = require('util');
+
+const writeFileAsync = promisify(fs.writeFile);
+
+let i = 0;
+
+const condition = (_block: any) => {
+  i += 1;
+  return i > 10;
+};
+
+async function run() {
+  for await (const block of fetchUntil(condition)) {
+    log.info(`Fetched block ${block.number}`);
+    await writeFileAsync(`${block.number}.json`, JSON.stringify(block));
+  }
+}
+
+run();
