9cd464eb5ba7c27b110aedf9bc15c8d119afad9c
Domagoj <djercic@gmail.com>
2021-10-14 22:07:17 +0200

    Add upsert method

--- src/entities/BlockEntity.ts
+++ src/entities/BlockEntity.ts
@@ -1,17 +1,23 @@
-import { Entity, PrimaryColumn, Column } from 'typeorm';
+import { Entity, PrimaryColumn, Column, Index } from 'typeorm';
 
 @Entity({ name: 'block' })
 export class BlockEntity {
   @PrimaryColumn()
   number: number;
+
   @Column({ name: 'gas_limit' })
   gasLimit: number;
+
   @Column({ name: 'gas_used' })
   gasUsed: number;
+
   @Column()
   miner: string;
+
   @Column()
   hash: string;
+
+  @Index()
   @Column()
   timestamp: number;
 }
--- src/repositories/blockRepository.ts
+++ src/repositories/blockRepository.ts
@@ -1,4 +1,4 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, Repository, DeepPartial } from 'typeorm';
 
 import { BlockEntity } from '../entities/BlockEntity';
 
@@ -15,5 +15,18 @@ export class BlockRepository extends Repository<BlockEntity> {
       .where('block.timestamp > :start', { start })
       .andWhere('block.timestamp < :end', { end })
       .getMany();
+  }
+
+  async upsert<T extends DeepPartial<BlockEntity>>(block: T) {
+    /**
+     * Save entity into the database if the entity does not exists.
+     * If entity is saved return true, if not, return false.
+     */
+    const existingBlock = await this.findOne(block);
+    if (existingBlock) {
+      return false;
+    }
+    await this.save(block);
+    return true;
   }
 }
