da2f30ab5add72fb9742f12d4d617a13b5e28ec2
Domagoj <djercic@gmail.com>
2021-10-14 10:54:22 +0200

    Load conifg

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import fs from 'fs/promises';
 import * as dbService from './services/db.service';
 import { fetchUntil } from './services/ethereum.service';
 import log from './services/logging.service';
+import { autoload } from './config';
 
 log.info('Starting ethereum stats service');
 
 const SUNDAY_OCT_10 = 1633824000;
 
 async function run() {
+  autoload();
   await dbService.setup();
   const blocks = fetchUntil((block) => block.timestamp <= SUNDAY_OCT_10);
 
