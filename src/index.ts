cc701cb1243a728f744ae2a1fd9a06e104072c1b
Domagoj <djercic@gmail.com>
2021-10-15 11:44:30 +0200

    Add subscribe and sync
    Subscribe for live updates from the ethereum blockchain

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,17 @@
 import { autoload } from './config';
 import * as dbService from './services/db.service';
-import { fetchUntil } from './services/ethereum.service';
+import { fetchUntil, subscribe } from './services/ethereum.service';
 import log from './services/logging.service';
 import { getCustomRepository } from 'typeorm';
 import { BlockRepository } from './repositories/BlockRepository';
-
-log.info('Starting ethereum stats service');
 
 async function syncUntilLastDatabaseEntry() {
   const blockRepo = getCustomRepository(BlockRepository);
 
   const latestBlock = await blockRepo.findLatest();
   const iterateBlocks = fetchUntil(
-    (block) => block.number !== latestBlock.number
+    (block) => block.number === latestBlock.number
   );
-
   for await (const block of iterateBlocks) {
     log.info(`Fetched block ${block.number}`);
     await blockRepo.save(block);
@@ -22,10 +19,30 @@ async function syncUntilLastDatabaseEntry() {
   }
 }
 
+async function onNewBlock(block) {
+  log.info(`New block ${block.number}`);
+  const blockRepo = getCustomRepository(BlockRepository);
+  const saved = await blockRepo.upsert(block);
+  if (saved) {
+    log.info(`Syncing block ${block.number}`);
+  } else {
+    log.warn(`Block ${block.number} already exists`);
+  }
+}
+
+async function onNewBlockError(err: Error) {
+  log.error(err);
+  throw err;
+}
+
 async function main() {
   autoload();
   await dbService.setup();
 
+  log.info('Starting ethereum stats service');
+  subscribe(onNewBlock, onNewBlockError);
+
+  log.info('Syncing until last timestamp');
   await syncUntilLastDatabaseEntry();
 }
 
