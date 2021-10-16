51e6bbbef37af0b74e090bd68f3d653ab2daa2c1
DJercic <djercic@gmail.com>
2021-10-16 13:52:07 +0200

    Add small util for current needs

---
+++ src/type/time.ts
@@ -0,0 +1,30 @@
+import moment from 'moment-timezone';
+
+export type StartEnd = {
+  start: number;
+  end: number;
+};
+
+export function dayTimestamps(date: Date): StartEnd {
+  /**
+   * Return start and end timestamps for a day in seconds. Date is transformed in UTC timezone.
+   */
+
+  const d = moment(date).utc();
+  const start = d.startOf('day').unix();
+  const end = d.endOf('day').unix();
+
+  return { start, end };
+}
+
+export function previousDay(): Date {
+  /**
+   * Return Date of the previous day when function is called. Use now as relative point in time.
+   */
+  const dateNow = moment.now();
+  return moment(dateNow).subtract(1, 'days').toDate();
+}
+
+export function previousDayTimestamps(): StartEnd {
+  return dayTimestamps(previousDay());
+}
