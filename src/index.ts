eb6ba7c0081cd887c89ed157a1a154d71f91d02d
Domagoj <djercic@gmail.com>
2021-10-18 16:30:13 +0200

    Extract out to sync

--- src/index.ts
+++ src/index.ts
@@ -1,74 +1,9 @@
-import * as cron from 'node-cron';
-import { getCustomRepository } from 'typeorm';
-
-import { autoload } from './config';
-import { BlockRepository } from './repositories/blockRepository';
+import { autoload, read } from './config';
 import * as dbService from './services/db.service';
-import * as ethService from './services/ethereum.service';
-import log from './services/logging.service';
-import * as time from './type/time';
+import * as sync from './sync';
 
-async function syncBlockDatabase() {
-  const blockRepo = getCustomRepository(BlockRepository);
-
-  const { end } = time.previousDayTimestamps();
-  const latestBlock = await blockRepo.findLatest();
-  /**
-   * If the database is empty fetch block for today.
-   * If there is something in the db fetch until last entry.
-   */
-  const condition = latestBlock
-    ? (block) => block.number === latestBlock.number
-    : (block) => block.timestamp <= end;
-
-  const iterateBlocks = ethService.fetchUntil(condition);
-  for await (const block of iterateBlocks) {
-    log.info(`Fetched block ${block.number}`);
-    await blockRepo.save(block);
-    log.info(`Synced block ${block.number}`);
-  }
-}
-
-async function onNewBlock(block) {
-  if (!block) {
-    // This case popped up while listening to Ethereum blockchain,
-    // although the typings suggest that an empty block is not possible.
-    log.error('Received an empty block');
-  }
-  log.info(`New block ${block.number}`);
-  const blockRepo = getCustomRepository(BlockRepository);
-  const saved = await blockRepo.upsert(block);
-  if (saved) {
-    log.info(`Synced block ${block.number}`);
-  } else {
-    log.warn(`Block ${block.number} already exists`);
-  }
-}
-
-async function onNewBlockError(err: Error) {
-  log.error(err);
-  throw err;
-}
-
-async function writeStatsToContract() {
-  const { start, end } = time.previousDayTimestamps();
-  const blockRepo = getCustomRepository(BlockRepository);
-  const stats = await blockRepo.calculateStatsBetween(start, end);
-  await ethService.depositStats(stats.count, stats.sum, '2021-10-18');
-}
-
-async function main() {
+(async function main() {
   autoload({ path: '.env' });
   await dbService.setup();
-
-  log.info('Starting ethereum stats service');
-  // Subscribe on live blocks from Ethereum network
-  ethService.subscribe(onNewBlock, onNewBlockError);
-
-  log.info('Syncing until last timestamp');
-
-  await syncBlockDatabase();
-  cron.schedule('* * * * *', writeStatsToContract, { timezone: 'UTC' });
-}
-
-main();
+  sync.run(read('CRON_SCHEDULE', '1 0 * * *'));
+})();
---
+++ src/sync.ts
@@ -0,0 +1,81 @@
+import * as cron from 'node-cron';
+
+import * as ethService from './services/ethereum.service';
+import * as time from './type/time';
+
+import { getCustomRepository } from 'typeorm';
+import { BlockRepository } from './repositories/blockRepository';
+import log from './services/logging.service';
+
+async function syncBlockDatabase() {
+  /**
+   * Sync block database until last entry is found in the database. If
+   * this is a first start, sync the blocks until yesterday.
+   */
+  const blockRepo = getCustomRepository(BlockRepository);
+  const latestBlock = await blockRepo.findLatest();
+
+  const { end } = time.previousDayTimestamps();
+  const condition = latestBlock
+    ? (block) => block.number === latestBlock.number
+    : (block) => block.timestamp <= end;
+
+  const iterateBlocks = ethService.fetchUntil(condition);
+  for await (const block of iterateBlocks) {
+    log.info(`Fetched block ${block.number}`);
+    await blockRepo.save(block);
+    log.info(`Synced block ${block.number}`);
+  }
+}
+
+async function onNewBlock(block) {
+  if (!block) {
+    // This case popped up while listening to Ethereum blockchain,
+    // although the typings suggest that an empty block is not possible.
+    log.error('Received an empty block');
+  }
+  log.info(`New block ${block.number}`);
+  const blockRepo = getCustomRepository(BlockRepository);
+  const saved = await blockRepo.upsert(block);
+  if (saved) {
+    log.info(`Synced block ${block.number}`);
+  } else {
+    log.warn(`Block ${block.number} already exists`);
+  }
+}
+
+async function onNewBlockError(err: Error) {
+  log.error(err);
+  throw err;
+}
+
+async function storeYesterdaysStatsToContract() {
+  try {
+    const { start, end } = time.previousDayTimestamps();
+    const blockRepo = getCustomRepository(BlockRepository);
+    const stats = await blockRepo.calculateStatsBetween(start, end);
+    await ethService.storeStats(
+      stats.count,
+      stats.sum,
+      time.toPreferredDateFormat(time.previousDay())
+    );
+  } catch (e) {
+    log.error(e);
+  }
+}
+
+export async function run(cronSchedule: string) {
+  /**
+   *
+   */
+  log.info('Subscribe to blockchain updates');
+  ethService.subscribe(onNewBlock, onNewBlockError);
+
+  log.info('Syncing until last timestamp');
+  await syncBlockDatabase();
+
+  log.info(`Starting cron schedule ${cronSchedule}`);
+  cron.schedule(cronSchedule, storeYesterdaysStatsToContract, {
+    timezone: 'UTC',
+  });
+}
